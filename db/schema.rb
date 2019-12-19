a4f3ea6dcd3f269b31a80c74cf79444b3624fa25
thsvr <vr.ths.zd@gmail.com>
2019-12-19 18:35:27 -0300

    added an Event model and created a foreign key to connect users table to events table creating a custom name association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_200312) do
+ActiveRecord::Schema.define(version: 2019_12_19_205908) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "description"
+    t.string "location"
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_12_19_200312) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "users"
 end
