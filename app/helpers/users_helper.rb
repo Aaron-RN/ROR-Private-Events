e42835471c9f1192b996419f42de8c7cfe47a593
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-12-20 15:19:22 -0500

    modified the User's show page view to categorize the attended events into past and upcoming events

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,16 @@
 module UsersHelper
+  # returns the user object of the currently logged-in user
   def current_user
-   if session[:user_id]
-    return User.find(session[:user_id])
-   end
+   return User.find(session[:user_id]) if session[:user_id]
    false
+  end
+
+  # returns true if the date given has already past
+  def event_past?(date)
+    return unless date
+
+    return true if Date.today > date
+
+    false
   end
 end
