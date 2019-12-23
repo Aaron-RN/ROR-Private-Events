17c50b121ae1dffbc8d1eae17ea29afec5f3509e
thsvr <vr.ths.zd@gmail.com>
2019-12-23 16:55:40 -0300

    added instance variables, @past_events and @upcoming_events, which shows the association between users and their attended events using scopes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @created_events = @user.events 
+    @past_events = @user.attended_events.past_events
+    @upcoming_events = @user.attended_events.upcoming_events
   end
 
 
